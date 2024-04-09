03534ae8b68a54235711d9229f08e97d86b704a4
kl1ps3r <seb1n22@soton.ac.uk>
2024-04-09 13:04:58 +0100

    Morning's work from 09/04, added finalised figures to create final graphs for justifying mask detector distance set at 1m for now. added code to calculate solid angle for a trapezoid aperture. added code to calculate surface area of shielding and then the mass of it.

--- parameters.py
+++ parameters.py
@@ -3,18 +3,22 @@ import numpy as np
 
 
 def Fully_Coded(d_m, d_d, D):
-    FCFOV = np.rad2deg(2 * np.arctan((d_m - d_d) / (2*D)))
+    FCFOV = 2 * np.arctan((d_m - d_d) / (2*D))
+
     return FCFOV
 
 def Partially_Coded(d_m, d_d, D):
-    PCFOV = np.rad2deg(2 * np.arctan((d_m + d_d) / (2*D)))
+    PCFOV = 2 * np.arctan((d_m + d_d) / (2*D))
     return PCFOV
 
 def AngRes(l_m, D):
-    AR = np.rad2deg(np.arctan(l_m / D))
+    AR = np.arctan(l_m / D)
     return AR
 
 def Localisation(l_d, D, sigma):
-    PSL = np.rad2deg((1 / sigma) * np.arctan(l_d / D))
+    PSL = (1 / sigma) * np.arctan(l_d / D)
 
     return PSL
+
+def SolidAngle(angle1, angle2=None):
+    return 4 * np.arcsin(np.sin(angle1 / 2) * np.sin(angle2 / 2))
